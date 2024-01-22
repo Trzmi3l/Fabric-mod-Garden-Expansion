f25d975eb874a859406d6be68239330c00fe86d6
DESKTOP-6BP6F5P\pylom <pylo.minecraft@gmail.com>
2024-01-22 14:53:26 +0100

    wszystko dzialajace czysty template

---
+++ src/main/java/com/gex/GardenexpansionClient.java
@@ -0,0 +1,10 @@
+package com.gex;
+
+import net.fabricmc.api.ClientModInitializer;
+public class GardenexpansionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
