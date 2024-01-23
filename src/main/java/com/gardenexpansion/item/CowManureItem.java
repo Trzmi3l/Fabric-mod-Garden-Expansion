bc47264b6f0334015181aabfe5606af01b5a520d
Keameleek <kermil.m@gmail.com>
2024-01-23 23:35:07 +0100

    krowie gowno skocnzone

--- src/main/java/com/gardenexpansion/item/CowManureItem.java
+++ src/main/java/com/gardenexpansion/item/CowManureItem.java
@@ -5,51 +5,53 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 
-public class CowManureItem extends BoneMealItem {
+import javax.swing.*;
+
+public class CowManureItem extends Item {
 
     static Settings s = new Settings();
     public static String ITEM_NAME = "cow_manure";
     public CowManureItem() {
 
         super(s);
+
     }
 
-/*
+
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
 
         if(!context.getWorld().isClient()) {
-            BlockPos posClick = context.getBlockPos();
-            PlayerEntity playerEntity = context.getPlayer();
+           BlockState bPos = context.getWorld().getBlockState(context.getBlockPos());
+           Block block = bPos.getBlock();
+            if(bPos.isIn(BlockTags.CROPS)) {
+                if (block instanceof Fertilizable fertilizable) {
 
-            BlockState state = context.getWorld().getBlockState(posClick);
+                    if (fertilizable.isFertilizable(context.getWorld(), context.getBlockPos(), bPos)) {
+                        if (context.getWorld() instanceof ServerWorld) {
+                            if (fertilizable.canGrow(context.getWorld(), context.getWorld().random, context.getBlockPos(), bPos)) {
+                                fertilizable.grow((ServerWorld) context.getWorld(), context.getWorld().random, context.getBlockPos(), bPos);
+                            }
+                            // context.getPlayer().getEquippedStack().getItem().getDefaultStack().decrement(1);
+                            context.getStack().decrement(1);
 
-
-            // TODO: OBSLUGE TEGO GOWNA
-
-
-
-            if(state.isOf(Blocks.FARMLAND)) {
-                Random rnd = Random.create();
-                state.randomTick((ServerWorld) context.getWorld(), posClick, rnd);
-
-
-
-                return ActionResult.SUCCESS;
+                            return ActionResult.SUCCESS;
+                        }
+                    }
+                }
             }
-
         }
 
 
         return ActionResult.FAIL;
     }
 
- */
 
 
 
