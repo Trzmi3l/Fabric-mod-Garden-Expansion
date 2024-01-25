14cf77e4a335f0e00235c1994b1a533d3fa57723
Keameleek <kermil.m@gmail.com>
2024-01-25 22:39:14 +0100

    owolib integratiojn, teraz jest kompletnie inna zasada rejestracji itemkow bo jest po prsoptu lepsza i zautomatyzowana, sperma

--- src/main/java/com/gardenexpansion/Gardenexpansion.java
+++ src/main/java/com/gardenexpansion/Gardenexpansion.java
@@ -1,9 +1,16 @@
 package com.gardenexpansion;
 
+import com.gardenexpansion.item.ItemsReg;
 import com.gardenexpansion.item.ModItemGroups;
 import com.gardenexpansion.item.ModItems;
+import io.wispforest.owo.itemgroup.Icon;
+import io.wispforest.owo.itemgroup.OwoItemGroup;
+import io.wispforest.owo.registration.reflect.FieldRegistrationHandler;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +18,15 @@ public class Gardenexpansion implements ModInitializer {
 	public static final String MOD_ID = "gardenexpansion";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final OwoItemGroup ITEM_GROUP = OwoItemGroup
+			.builder(new Identifier(Gardenexpansion.MOD_ID, "default"), () -> Icon.of(Items.FLOWERING_AZALEA))
+			.build();
+
+
 	@Override
 	public void onInitialize() {
+		ITEM_GROUP.initialize();
 
-		ModItemGroups.registerItemGroups();
-		ModItems.registerModItems();
+		FieldRegistrationHandler.register(ItemsReg.class, MOD_ID, false);
 	}
 }
--- src/main/java/com/gardenexpansion/item/ModItemGroups.java
+++ src/main/java/com/gardenexpansion/item/ModItemGroups.java
@@ -2,22 +2,32 @@ package com.gardenexpansion.item;
 
 import com.gardenexpansion.Gardenexpansion;
 import com.gardenexpansion.item.CowManureItem;
+import io.wispforest.owo.itemgroup.Icon;
+import io.wispforest.owo.itemgroup.OwoItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.block.FlowerBlock;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-
+@Deprecated
 public class ModItemGroups {
 
+    /*
     public static final ItemGroup GEX_GROUP = Registry.register(Registries.ITEM_GROUP,
             new Identifier(Gardenexpansion.MOD_ID, "gexgroup"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.gexgroup"))
-                    .icon(() -> new ItemStack(ModItems.COW_MANURE)).entries((displayContext, entries) -> {
+                    .icon(() -> new ItemStack(ItemsReg.COW_MANURE)).entries((displayContext, entries) -> {
             }).build());
     public static void registerItemGroups(){
         Gardenexpansion.LOGGER.info("Registering Creative Item Groups for: "+Gardenexpansion.MOD_ID);
     }
+
+     */
+
+
+
 }
