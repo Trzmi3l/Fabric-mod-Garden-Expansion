82c36b2ccf0aa51b48e287444fc493cd6f5ae8d5
DESKTOP-R2CQNTS\LO_PRO <kermil.m@gmail.ccom>
2024-01-26 07:52:17 +0100

    blockreg i wgl jakeis goofy rzeczy pododawalem

---
+++ src/main/java/com/gardenexpansion/block/WaterCollectorBlock.java
@@ -0,0 +1,19 @@
+package com.gardenexpansion.block;
+
+import com.gardenexpansion.Gardenexpansion;
+
+import net.minecraft.block.Block;
+
+
+
+public class WaterCollectorBlock extends Block {
+
+    public static final String BLOCK_NAME = "Water collector";
+
+    public WaterCollectorBlock(Settings settings) {
+        super(settings);
+    }
+
+
+
+}
