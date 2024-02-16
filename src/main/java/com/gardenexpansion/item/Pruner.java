d7dd1af4549561b151aa5f1a4a124ebde56960d4
DESKTOP-R2CQNTS\LO_PRO <kermil.m@gmail.ccom>
2024-02-16 08:13:59 +0100

    opruner :33

---
+++ src/main/java/com/gardenexpansion/item/Pruner.java
@@ -0,0 +1,38 @@
+package com.gardenexpansion.item;
+
+import com.gardenexpansion.Gardenexpansion;
+import io.wispforest.owo.itemgroup.OwoItemSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.ToolItem;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+
+import javax.tools.Tool;
+
+public class Pruner extends Item {
+
+    static OwoItemSettings s = new OwoItemSettings().group(Gardenexpansion.ITEM_GROUP).maxDamageIfAbsent(400);
+
+    public Pruner() {
+        super(s);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+
+        if(!context.getWorld().isClient) {
+            BlockPos bp = context.getBlockPos();
+            BlockState bs = context.getWorld().getBlockState(bp);
+
+            if(bs.isIn(BlockTags.FLOWERS) || bs.isIn(BlockTags.SMALL_FLOWERS) || bs.isIn(BlockTags.TALL_FLOWERS)) {
+                
+            }
+        }
+
+        return ActionResult.PASS;
+    }
+}
