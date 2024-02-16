201237abbd665d45b3facbcbb5ef52b0ce3556b1
DESKTOP-R2CQNTS\LO_PRO <kermil.m@gmail.ccom>
2024-02-16 09:11:40 +0100

    dfhdfhfhddfhhhfhdfdfhddfhdvb cvbdfgdrgdfsfdsefsdhxjmfkrftyhserhbrsefseyhsesdgvxcjdfjdfhdvfncvmn

--- src/main/java/com/gardenexpansion/item/ModItemGroups.java
+++ src/main/java/com/gardenexpansion/item/ModItemGroups.java
@@ -19,47 +19,24 @@ public class ModItemGroups {
 
 
     public static final ItemGroup GEX_GROUP = Registry.register(Registries.ITEM_GROUP,
-<<<<<<< HEAD
 
-=======
->>>>>>> 070f6f4299f3acfc868ccc3bb1de894c9f5d077b
-            new Identifier(Gardenexpansion.MOD_ID, "gardenexpansion"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemGroup.gardenexpansion.default"))
-
-                    .icon(() -> new ItemStack(Items.ACACIA_FENCE_GATE)).entries((displayContext, entries) -> {
-<<<<<<< HEAD
 
             new Identifier(Gardenexpansion.MOD_ID, "gexgroup"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.gexgroup"))
                     .icon(() -> new ItemStack(new CowManureItem())).entries((displayContext, entries) -> {
 
-=======
-            new Identifier(Gardenexpansion.MOD_ID, "gexgroup"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.gexgroup"))
-                    .icon(() -> new ItemStack(ModItems.COW_MANURE)).entries((displayContext, entries) -> {
->>>>>>> 070f6f4299f3acfc868ccc3bb1de894c9f5d077b
 
                     //tutaj mozna wpierdalac rzeczy do Gex grupki i w takiej kolejnosci jak zrobisz enrty w takiej beda w zakladce :3
 
                         entries.add(new CowManureItem()); // krwoie gowno
 
 
-<<<<<<< HEAD
-
-                    })
-
-
-
-                    ).build());
-
-=======
-                    })
-
-                    .icon(() -> new ItemStack(ItemsReg.COW_MANURE)).entries((displayContext, entries) -> {
-            }).build());
                     }).build());
->>>>>>> origin/pylo_majkraft_odnoga
->>>>>>> 070f6f4299f3acfc868ccc3bb1de894c9f5d077b
+
+
+
+
+
 
     public static void registerItemGroups(){
         Gardenexpansion.LOGGER.info("Registering Creative Item Groups for: "+Gardenexpansion.MOD_ID);
