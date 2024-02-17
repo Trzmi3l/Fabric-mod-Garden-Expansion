852bca6910e3225a03aeb1d7e93e19d8ef3bf89d
przepychacz <pylo.minecraft@gmail.com>
2024-02-17 22:45:53 +0100

    ide zrobic sobie kisiel

--- src/main/java/com/gardenexpansion/item/RegisterItems.java
+++ src/main/java/com/gardenexpansion/item/RegisterItems.java
@@ -1,11 +1,14 @@
 package com.gardenexpansion.item;
 
+import com.gardenexpansion.Gardenexpansion;
+import io.wispforest.owo.itemgroup.OwoItemSettings;
 import io.wispforest.owo.registration.reflect.ItemRegistryContainer;
 import net.minecraft.item.Item;
 
 public class RegisterItems implements ItemRegistryContainer {
     public static final Item COW_MANURE = new CowManureItem();
     public static final Item OAK_BARK = new OakBarkItem();
+    public static final Item SHIT_INGOT = new Item(new OwoItemSettings().group(Gardenexpansion.ITEM_GROUP));
 /*
     public Item createItem(Item item, String Identifier) {
         return new Item(new OwoItemSettings().group(Gardenexpansion.ITEM_GROUP));
