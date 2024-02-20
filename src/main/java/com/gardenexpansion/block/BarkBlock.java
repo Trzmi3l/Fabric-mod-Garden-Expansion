059ebb690ff7217f5be1964167c5493fb085f9e1
przepychacz <pylo.minecraft@gmail.com>
2024-02-20 13:49:11 +0100

    srac mi sie chce

--- src/main/java/com/gardenexpansion/block/BarkBlock.java
+++ src/main/java/com/gardenexpansion/block/BarkBlock.java
@@ -1,31 +1,18 @@
 package com.gardenexpansion.block;
 
-import io.wispforest.owo.itemgroup.OwoItemSettings;
-import net.fabricmc.fabric.api.block.v1.FabricBlock;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.renderer.v1.material.MaterialFinder;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockTypes;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.MiningToolItem;
-import net.minecraft.item.ShovelItem;
-import net.minecraft.item.ToolMaterials;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Identifier;
 
 
 public class BarkBlock extends Block {
 
     public static final BlockSoundGroup BARK_BLOCK_SOUNDS = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_MANGROVE_ROOTS_BREAK, SoundEvents.BLOCK_WART_BLOCK_STEP, SoundEvents.BLOCK_WART_BLOCK_PLACE, SoundEvents.BLOCK_WART_BLOCK_HIT, SoundEvents.BLOCK_WART_BLOCK_FALL);
-    static FabricBlockSettings BlockSettings = FabricBlockSettings.copyOf(Blocks.DIRT).sounds(BARK_BLOCK_SOUNDS);
+    static FabricBlockSettings BarkBlockSettings = FabricBlockSettings.copyOf(Blocks.DIRT).sounds(BARK_BLOCK_SOUNDS);
 
     public BarkBlock() {
-        super(BlockSettings);
+        super(BarkBlockSettings);
     }
 }
