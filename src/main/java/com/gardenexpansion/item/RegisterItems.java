7e600d327f64d6a0423872db0c9f5ae291019e77
przepychacz <pylo.minecraft@gmail.com>
2024-02-22 17:42:08 +0100

    test :3

--- src/main/java/com/gardenexpansion/item/RegisterItems.java
+++ src/main/java/com/gardenexpansion/item/RegisterItems.java
@@ -9,7 +9,7 @@ public class RegisterItems implements ItemRegistryContainer {
     public static final Item COW_MANURE = new CowManureItem();
     public static final Item OAK_BARK = new OakBarkItem();
     public static final Item SHIT_INGOT = new Item(new OwoItemSettings().group(Gardenexpansion.ITEM_GROUP));
-    public static final Item PRUNER = new PrunerItem();
+    public static final Item PRUNER = new PrunerItem(); //sranie rzytnia 
 /*
     public Item createItem(Item item, String Identifier) {
         return new Item(new OwoItemSettings().group(Gardenexpansion.ITEM_GROUP));
