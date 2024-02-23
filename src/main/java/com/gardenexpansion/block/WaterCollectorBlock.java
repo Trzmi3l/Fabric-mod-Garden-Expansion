f59b6b5517a6907e383e2b625bea1a734ffaff09
DESKTOP-R2CQNTS\LO_PRO <kermil.m@gmail.ccom>
2024-02-23 08:04:38 +0100

    dupa

--- src/main/java/com/gardenexpansion/block/WaterCollectorBlock.java
+++ src/main/java/com/gardenexpansion/block/WaterCollectorBlock.java
@@ -3,17 +3,19 @@ package com.gardenexpansion.block;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.datafixer.fix.ChunkPalettedStorageFix;
-import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.DirectionProperty;
-import net.minecraft.state.property.Properties;
-import net.minecraft.util.BlockMirror;
-import net.minecraft.util.BlockRotation;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.function.BooleanBiFunction;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.World;
 
 import java.util.stream.Stream;
 
@@ -23,24 +25,34 @@ public class WaterCollectorBlock extends Block {
     public static final String BLOCK_NAME = "water_collector";
     static FabricBlockSettings s = FabricBlockSettings.create().strength(3);
 
-    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+    public static final BooleanProperty IS_FILLED = BooleanProperty.of("full");
 
     public WaterCollectorBlock() {
         super(s);
+        setDefaultState(getDefaultState().with(IS_FILLED, false));
     }
 
     private static final VoxelShape d = Stream.of(
-            Block.createCuboidShape(2, 1, 2, 3, 7, 3),
-            Block.createCuboidShape(12, -5, 2, 13, 1, 3),
-            Block.createCuboidShape(12, -5, 13, 13, 1, 14),
-            Block.createCuboidShape(2, 1, 13, 3, 7, 14),
-            Block.createCuboidShape(5, 5, 2, 11, 6, 14),
-            Block.createCuboidShape(4, 5, 1, 5, 7, 15),
-            Block.createCuboidShape(11, 5, 1, 12, 7, 15),
-            Block.createCuboidShape(5, 5, 1, 11, 7, 2),
-            Block.createCuboidShape(5, 5, 14, 11, 7, 15),
-            Block.createCuboidShape(5, 6, 2, 11, 7, 14)
-            ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+            Block.createCuboidShape(1, 0, 1, 15, 1, 15),
+            Block.createCuboidShape(1, 1, 1, 3, 5, 3),
+            Block.createCuboidShape(1, 1, 13, 3, 5, 15),
+            Block.createCuboidShape(13, 1, 1, 15, 5, 3),
+            Block.createCuboidShape(13, 1, 13, 15, 5, 15),
+            Block.createCuboidShape(3, 1, 1, 13, 5, 2),
+            Block.createCuboidShape(3, 1, 14, 13, 5, 15),
+            Block.createCuboidShape(14, 1, 3, 15, 5, 13),
+            Block.createCuboidShape(2, 4, 2, 14, 4, 14),
+            Block.createCuboidShape(1, 1, 3, 2, 5, 13)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
 
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        world.setBlockState(pos, state.with(IS_FILLED,true));
+        super.onSteppedOn(world, pos, state, entity);
+    }
 
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(IS_FILLED);
+    }
 }
