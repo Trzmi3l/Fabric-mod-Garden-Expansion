3b4763800cb7154e1e122cd74c1bcb39b3184dd6
pylo_minecrat na uchodztwie\LO_PRO <pylo.minecraft@gmail.com>
2024-03-08 09:23:04 +0100

    stejd

--- src/main/java/com/gardenexpansion/datagen/ItemTagProvider.java
+++ src/main/java/com/gardenexpansion/datagen/ItemTagProvider.java
@@ -1,4 +1,20 @@
 package com.gardenexpansion.datagen;
 
-public class ItemTagProvider {
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ItemTagProvider extends FabricTagProvider.ItemTagProvider {
+
+    public ItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup arg) {
+
+    }
 }
