1ea4858b60f615543831c07e6dc7f8698a6c6e4f
DESKTOP-R2CQNTS\LO_PRO <kermil.m@gmail.ccom>
2024-04-05 07:57:17 +0200

    troche poprwione glpotki

--- src/main/java/com/gardenexpansion/Gardenexpansion.java
+++ src/main/java/com/gardenexpansion/Gardenexpansion.java
@@ -38,24 +38,4 @@ public class Gardenexpansion implements ModInitializer {
 		FieldRegistrationHandler.register(BlockEntityRegister.class, MOD_ID,false);
 
 
-		LOGGER.info("\n"+"Policjant mnie dusi, strach ściska mi gardło,\n" +
-				"A w myślach myśl jedna: \"Czy to już piekło?\".\n" +
-				"Lecz nagle ocknienie, mixiny działają!\n" +
-				"I znika oprawca, a ja znów oddycham.\n" +
-				"\n" +
-				"W euforii radości, śmiech mój się rozlega,\n" +
-				"Lecz nagle czuję, że coś jest nie tak.\n" +
-				"W majtkach bulgocze, a smród uderza w nozdrza,\n" +
-				"Nasrałem do chleba! Cóż za paskudna zbrodnia!\n" +
-				"\n" +
-				"Teraz to już pewne, piekło mnie czeka,\n" +
-				"Za gówno w chlebie i ucieczkę od pecha.\n" +
-				"Lecz mixiny działają, więc może jeszcze jest szansa,\n" +
-				"Na odkupienie i życie bez smrodu.\n" +
-				"\n" +
-				"Więc ruszam przed siebie, z nadzieją w sercu,\n" +
-				"Że mixiny mnie uratują od piekielnej czeluści.\n" +
-				"I może kiedyś jeszcze zaznam szczęścia,\n" +
-				"Bez smrodu gówna i policyjnej pałki.");
-	}
 }
